5676dcc81faef0311a9c5c566b850f245284df3a
Mikhail <mehan@list.ru>
2022-09-05 16:41:04 +0300

    Task is done

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -1,1 +1,16 @@
 ﻿
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int[] numbers = new int[5];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = Prompt($"Введите {i}-е число: ");
+}
+
+System.Console.WriteLine($"Сумма всех элементов массива равна {numbers.Sum()}, а их среднее арефмитическое равно {numbers.Average()}");
