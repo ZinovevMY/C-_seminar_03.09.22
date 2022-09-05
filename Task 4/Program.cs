87097d11d47e7fd946423d2b3394a4fe6689785e
Mikhail <mehan@list.ru>
2022-09-05 16:48:02 +0300

    Task is done

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int number = Prompt("Введите натуральное целое число: ");
+if (number%7 == 0 && number%23 == 0)
+{
+    System.Console.WriteLine("Введенное число кратно 7 и 23.");
+}
+else
+{
+    System.Console.WriteLine("Введенное число одновременно не кратно 7 и 23.");
+}
