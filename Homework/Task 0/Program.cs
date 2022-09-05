ae6a5c19ff7800f78f279fe10bfd3a22ddc8274b
Mikhail <mehan@list.ru>
2022-09-05 16:59:38 +0300

    Task is done

--- Homework/Task 0/Program.cs
+++ Homework/Task 0/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки для расчета.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Prompt("Введите 3-значное целое число: ");
+
+System.Console.WriteLine($"Вторая цифра во введенном числе {number} равна {(number/10)%10}");
