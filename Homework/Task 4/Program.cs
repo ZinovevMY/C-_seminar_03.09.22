92f54e1b213b14a75d21d3ac07eeb1ef717eb7e1
Mikhail <mehan@list.ru>
2022-09-06 20:18:42 +0300

    Task is done

--- Homework/Task 4/Program.cs
+++ Homework/Task 4/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿//Напишите программу, которая генерирует несколько случайных чисел, и в цикле проверяет, кратны ли эти числа предварительно введенному числу, при кратности - цикл прерывается.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool NumberMultiplicity (int num1, int num2)
+{
+    if (num1%num2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int multipleNumber = Prompt("Введите число для проверки кратности: ");
+while (true)
+{
+    int number = new Random().Next(1,100);
+    if (NumberMultiplicity(number,multipleNumber))
+    {
+        System.Console.WriteLine($"Сгенерированное число {number} кратно введенному числу {multipleNumber}.");
+        break;
+    }
+    else
+    {
+        System.Console.WriteLine($"Сгенерированное число {number} не кратно введенному числу {multipleNumber}.");
+    }
+}
