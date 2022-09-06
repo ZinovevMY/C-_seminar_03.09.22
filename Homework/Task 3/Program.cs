8af28e7532dd65cbe9910243bc73fc11224d9769
Mikhail <mehan@list.ru>
2022-09-06 20:05:05 +0300

    Task is done

--- Homework/Task 3/Program.cs
+++ Homework/Task 3/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool IsHoliday(int num)
+{
+    if (num == 6 || num == 7)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int weekDay = Prompt("Введите день недели: ");
+if (weekDay < 1 || weekDay > 7)
+{
+    System.Console.WriteLine("Нет такого дня недели!");
+}
+else
+{
+    if (IsHoliday(weekDay))
+    {
+        System.Console.WriteLine("Это выходной!");
+    }
+    else
+    {
+        System.Console.WriteLine("Это будний день!");
+    }
+}
